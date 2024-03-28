bf0199e0132fa54d2a1bc3000910fd96981b7cca
Deepjyoti Das <deepdeepdasdasdas@gmail.com>
2024-03-29 02:58:29 +0530

    added cors

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-import express  from "express";
-import dbconnect  from "./db.js";
+import express from "express";
+import cors from "cors";
+import dbconnect from "./db.js";
 import router from "./routes/router.js";
 
-const app=express();
- 
+const app = express();
+
 // middlewares  
-app.use(express.json());  
-app.use("/api",router);  
-  
+app.use(express.json());
+app.use("/api", router);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // If you're using cookies or authorization headers
+}));
 // server listening
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`);
 });
 
