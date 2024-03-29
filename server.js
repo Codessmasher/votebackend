ebe502c2773d921b9e17167ac8eccf3ff90d31eb
Deepjyoti Das <deepdeepdasdasdas@gmail.com>
2024-03-29 18:31:21 +0530

    cors updated

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ import router from "./routes/router.js";
 const app = express();
 
 // middlewares  
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
-app.use(cors());
 // server listening
 app.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`);
