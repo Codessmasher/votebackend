853c861891ade787f04618833bcc9ad1061f154a
Deepjyoti Das <deepdeepdasdasdas@gmail.com>
2024-04-02 15:34:11 +0530

    Update hashing the password in the user object

--- controllers/users/updateuser.js
+++ controllers/users/updateuser.js
@@ -1,28 +1,17 @@
 import User from "../../models/user.js";
 import bcrypt from "bcryptjs";
-
 const updateuser = async (req, res) => {  
     try {
-        // Retrieve the user by ID
-        const user = await User.findById(req.params.id);
+        if (req.body.password) { 
+            // Update hashing the password in the user object 
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        } 
+        const updatedUser = await User.findByIdAndUpdate({_id:req.params.id}, req.body, { new: true });
 
-        // Check if the user exists
-        if (!user) {
+        if (!updatedUser) {
             return res.status(404).json({ error: "User not found" });
         }
-
-        // Check if the password needs to be updated
-        if (req.body.password) {
-            // Hash the new password
-            const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            // Update the password in the user object
-            user.password = hashedPassword;
-        } 
-
-        // Save the updated user
-        const updatedUser = await user.save();
-
-        // Return the updated user
+        
         return res.status(200).json({ message: "User updated successfully!", result: updatedUser });
     } catch (error) { 
         return res.status(500).json({ error: "Internal server error" });
